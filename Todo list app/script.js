75f098b5bf531cfb163bb4b83a9bc95bbe63ff43
niteshkr034 <81917957+niteshkr034@users.noreply.github.com>
2022-05-27 19:58:04 +0530

    first commit

---
+++ Todo list app/script.js
@@ -0,0 +1,37 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// event listeners
+todoButton.addEventListener('click', addTodo);
+
+
+
+// Functions
+function addTodo(event){
+    // prevent default submit behaviour
+    event.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create list
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    
+    // Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    // append todo list
+    todoList.appendChild(todoDiv)
+}
